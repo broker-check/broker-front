c019d04852fdda56a8e82472139f3a352561259d
Azamat Omirtai <omirtajazamat@gmail.com>
2025-08-21 23:31:05 +0500

    [refactor]://init - Vue 3 + Vite + Router + Pinia + Tailwind + Axios

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-6 text-center">
+    <h1 class="text-3xl font-bold text-blue-600">
+      🚀 Broker-Check работает!
+    </h1>
+    <p class="mt-4 text-gray-600">
+      Vue 3 + Vite + Vue Router + Pinia + Tailwind + Axios
+    </p>
+  </div>
+</template>
